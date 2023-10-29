f0b5af034f7550caf6ab0f5bcb7febb3c04db082
shinchan08113 <141140444+shinchan08113@users.noreply.github.com>
2023-10-29 09:06:34 +0530

    Delete java directory

--- java/Escape.java
+++
@@ -1,9 +0,0 @@
-public class Escape {
-    public static void main(String[] args)
-    {
-        System.out.println("Hi,\nWelcome Back Shinchan");
-        System.out.println("Welcomes to \"Bootcamp\" ");
-
-    }
-    
-}
