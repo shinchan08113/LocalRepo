d19a6c0976d985f6883aa25a25a36313316329e7
shinchan08113 <141140444+shinchan08113@users.noreply.github.com>
2023-10-29 09:06:11 +0530

    Delete c++ directory

--- c++/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Hello world.\n";
-    cout<<"This programm is made by shinchan.\n";
-    return 0;
-}
--- c++/operatorpre.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a=3,b=4,c;
-    c=((((a*1)+b)-45)+87);
-    cout<<c;
-    return 0;
-}
